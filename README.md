1d0820949e5a0d251a27e81f81a55217f26490bb
jmaloney@trevetts.com <jmaloney@trevetts.com>
2017-04-24 08:24:11 -0400

    update

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # context.deletermover
+
+'Mover' part is not working because of problems with Samba and Python I have yet to figure out.
+
 Kodi 17 delete or move tv shows and movies (keeps library clean)
--- addon.py
+++ addon.py
@@ -4,30 +4,56 @@ import os
 import xbmcgui
 import xbmcplugin
 import sys
+import ntpath
+
+
+
 
 def delete_file(file,title):
     
     
     
-    if xbmcgui.Dialog().yesno( "Confirm Delete", "Confirm Delete For " + title + watchedFolderTvShows +"??", "", "", "Cancel", "Delete" ):
+    if xbmcgui.Dialog().yesno( "Confirm " + action, "Confirm " + action + " For " + title  +"?", "", "", "Cancel", "Delete" ):
         if (currentPlayingFile==file):
             xbmc.executebuiltin('Notification(Item, ' + "Stopping video because it's playing" + ', 2)')
             xbmc.executebuiltin("PlayerControl(Stop)")
         if os.path.exists(file):
+            filename = ntpath.basename(file)
+            if ( action == 'Delete' ):
                 os.remove(file)
                 hbd = ' has been deleted'
-                xbmc.executebuiltin("XBMC.CleanLibrary(video)")
-                xbmc.executebuiltin('Notification(Item, ' + path + hbd + ', 2)')		
+            else:
+                xbmc.log('SOURCE: ' + file, xbmc.LOGERROR)
+                xbmc.log('TARGET: ' + watchedFolder  + filename, xbmc.LOGERROR)
+                target = watchedFolder + '/' + filename
+                #xbmc.executebuiltin('Notification(Item, ' + target + ', 2)')
+                renameResult = os.rename(file, watchedFolder +  filename)
+            xbmc.executebuiltin("XBMC.CleanLibrary(video)")
+            xbmc.executebuiltin('Notification(Item, ' + title + hbd + ', 3)')		
 		
  
 path = xbmc.getInfoLabel('ListItem.FileNameAndPath')
 title = xbmc.getInfoLabel('ListItem.Title')
+__settings__ = xbmcaddon.Addon(id="context.deletermover")
 
-mytext = str(sys.argv[0])
-xbmc.executebuiltin('Notification(Item, ' + mytext + ', 2)')
+if xbmc.getCondVisibility( 'VideoPlayer.Content(movies)' ):
+    currentContent = 'movies'
+    watchedFolder = __settings__.getSetting("watchedFolderMovies")
+else:
+    currentContent = 'tvshows'
+    watchedFolder = __settings__.getSetting("watchedFolderTvShows")
+    
+    
+watchedFolder = '' # Move wont work with Python so this is a delete-only thing (samba + windows problems...)
+    
+if (watchedFolder == ''):
+    action = 'Delete'
+else:
+    action = 'Move'
+    
 
-#watchedFolderMovies = xbmcaddon.getSetting("watchedFolderMovies")
-watchedFolderTvShows = xbmcplugin.getSetting(int(sys.argv[1]), 'watchedFolderTvShows')
+
+
 
 currentPlayingFile = xbmc.getInfoLabel("Player.Filenameandpath")
 delete_file(path,title)
